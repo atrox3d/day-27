16be250db44eb6fb3bbfe5fe2aaf3aa639952ba6
atrox3d <atrox3d@gmail.com>
2021-02-17 11:58:42 +0100

    add label, button

--- main.py
+++ main.py
@@ -5,9 +5,22 @@ window.title("my first GUI program")
 window.minsize(width=500, height=300)
 
 # label
+
 label = tkinter.Label(text="label", font=("Arial", 24, "bold"))
 # center-top
 label.pack()
-label.pack(side="left")
+label.pack()
+label["text"] = "new text"
+label.config(text="other text")
+
+
+# button
+
+def button_click():
+    label["text"] = "Click"
+
+
+button = tkinter.Button(text="button", command=button_click)
+button.pack()
 
 window.mainloop()
