b06be1aa093d2212355b0a38e1fe3b05248329c6
atrox3d <atrox3d@gmail.com>
2021-02-18 06:35:42 +0100

    using .place()

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ tkinter.TEXT = "text"
 # use it
 label[tkinter.TEXT] = "new text"
 label.config(text="other text")
-label.pack(side=tkinter.LEFT)
-
+# label.pack(side=tkinter.LEFT)
+label.place(x=0, y=0)
 # button
 
 button = tkinter.Button(text="button", command=button_click)
-button.pack(side=tkinter.TOP)
-
+# button.pack(side=tkinter.TOP)
+button.place(x=0, y=50)
 # entry
 
 entry = tkinter.Entry(width=10)
-entry.pack(side=tkinter.RIGHT)
-
+# entry.pack(side=tkinter.RIGHT)
+entry.place(x=0, y=100)
 window.mainloop()
