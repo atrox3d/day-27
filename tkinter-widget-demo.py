d167921ac6235aecc32349269f162851a2613432
atrox3d <atrox3d@gmail.com>
2021-02-18 05:37:49 +0100

    change import

--- tkinter-widget-demo.py
+++ tkinter-widget-demo.py
@@ -3,15 +3,16 @@
 #   https://repl.it/@appbrewery/tkinter-widget-demo
 #   https://repl.it/@RobyLombardo/tkinter-widget-demo#main.py
 #
-from tkinter import *
+# from tkinter import *
+import tkinter
 
 # Creating a new window and configurations
-window = Tk()
+window = tkinter.Tk()
 window.title("Widget Examples")
 window.minsize(width=500, height=500)
 
 # Labels
-label = Label(text="This is old text")
+label = tkinter.Label(text="This is old text")
 label.config(text="This is new text")
 label.pack()
 
@@ -22,25 +23,25 @@ def action():
 
 
 # calls action() when pressed
-button = Button(text="Click Me", command=action)
+button = tkinter.Button(text="Click Me", command=action)
 button.pack()
 
 # Entries
-entry = Entry(width=30)
+entry = tkinter.Entry(width=30)
 # Add some text to begin with
-entry.insert(END, string="Some text to begin with.")
+entry.insert(tkinter.END, string="Some text to begin with.")
 # Gets text in entry
 print(entry.get())
 entry.pack()
 
 # Text
-text = Text(height=5, width=30)
+text = tkinter.Text(height=5, width=30)
 # Puts cursor in textbox.
 text.focus()
 # Adds some text to begin with.
-text.insert(END, "Example of multi-line text entry.")
+text.insert(tkinter.END, "Example of multi-line text entry.")
 # Get's current value in textbox at line 1, character 0
-print(text.get("1.0", END))
+print(text.get("1.0", tkinter.END))
 text.pack()
 
 
@@ -50,7 +51,7 @@ def spinbox_used():
     print(spinbox.get())
 
 
-spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox = tkinter.Spinbox(from_=0, to=10, width=5, command=spinbox_used)
 spinbox.pack()
 
 
@@ -60,7 +61,7 @@ def scale_used(value):
     print(value)
 
 
-scale = Scale(from_=0, to=100, command=scale_used)
+scale = tkinter.Scale(from_=0, to=100, command=scale_used)
 scale.pack()
 
 
@@ -71,8 +72,8 @@ def checkbutton_used():
 
 
 # variable to hold on to checked state, 0 is off, 1 is on.
-checked_state = IntVar()
-checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state = tkinter.IntVar()
+checkbutton = tkinter.Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
 checked_state.get()
 checkbutton.pack()
 
@@ -83,9 +84,9 @@ def radio_used():
 
 
 # Variable to hold on to which radio button value is checked.
-radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radio_state = tkinter.IntVar()
+radiobutton1 = tkinter.Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = tkinter.Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
 radiobutton1.pack()
 radiobutton2.pack()
 
@@ -96,7 +97,7 @@ def listbox_used(event):
     print(listbox.get(listbox.curselection()))
 
 
-listbox = Listbox(height=4)
+listbox = tkinter.Listbox(height=4)
 fruits = ["Apple", "Pear", "Orange", "Banana"]
 for item in fruits:
     listbox.insert(fruits.index(item), item)
