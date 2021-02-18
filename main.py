780f22d55f05788bbe639d4f74f9c64200c5543a
atrox3d <atrox3d@gmail.com>
2021-02-18 07:05:41 +0100

    using .grid()

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter
 
 def button_click():
     label["text"] = entry.get()
+    # button.grid(row=0, column=5)
 
 
 window = tkinter.Tk()
@@ -18,15 +19,24 @@ tkinter.TEXT = "text"
 label[tkinter.TEXT] = "new text"
 label.config(text="other text")
 # label.pack(side=tkinter.LEFT)
-label.place(x=0, y=0)
+# label.place(x=0, y=0)
+label.grid(row=0, column=0)
+
 # button
 
 button = tkinter.Button(text="button", command=button_click)
 # button.pack(side=tkinter.TOP)
-button.place(x=0, y=50)
+# button.place(x=0, y=50)
+button.grid(row=1, column=1)
+
+
 # entry
 
 entry = tkinter.Entry(width=10)
 # entry.pack(side=tkinter.RIGHT)
-entry.place(x=0, y=100)
+# entry.place(x=0, y=100)
+entry.grid(column=2, row=2)
+
+
+
 window.mainloop()
